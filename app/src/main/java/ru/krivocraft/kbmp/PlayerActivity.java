dd3b6d5a31d34ee4154db8e6760adb0838f95bc6
kefir.fedorov <kefir.fedorov@gmail.com>
2018-09-08 21:56:26 +0300

    removed unnecessary check

--- app/src/main/java/ru/krivocraft/kbmp/PlayerActivity.java
+++ app/src/main/java/ru/krivocraft/kbmp/PlayerActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
@@ -160,12 +159,10 @@ public class PlayerActivity extends AppCompatActivity implements SeekBar.OnSeekB
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.play_pause:
-                if (mBounded) {
-                    if (!mService.isPlaying()) {
-                        startPlaying();
-                    } else {
-                        stopPlaying();
-                    }
+                if (!mService.isPlaying()) {
+                    startPlaying();
+                } else {
+                    stopPlaying();
                 }
                 break;
             case R.id.previous:
