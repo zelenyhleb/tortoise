30cc4d655456bea4a87e2a456054c7f59f0d4643
Nikifor Fedorov <zelenyhleb@gmail.com>
2019-07-19 18:17:20 +0300

    added close notification action

--- app/src/main/java/ru/krivocraft/kbmp/SettingsPage.java
+++ app/src/main/java/ru/krivocraft/kbmp/SettingsPage.java
@@ -9,13 +9,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SettingsPage extends Fragment {
 
