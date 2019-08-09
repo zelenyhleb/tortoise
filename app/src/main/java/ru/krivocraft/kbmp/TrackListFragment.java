647357d00ba47caea5a798960293b532afb79ada
Nikifor Fedorov <kefir.fedorov@gmail.com>
2019-08-09 13:49:06 +0300

    staged

--- app/src/main/java/ru/krivocraft/kbmp/TrackListFragment.java
+++ app/src/main/java/ru/krivocraft/kbmp/TrackListFragment.java
@@ -1,9 +1,7 @@
 package ru.krivocraft.kbmp;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -18,17 +16,15 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class TrackListFragment extends Fragment {
 
-    private List<String> trackList;
+    private TrackList trackList;
     private TrackAdapter adapter;
     private ListView listView;
     private boolean showControls;
-    private boolean startedWithList;
-    private ProgressBar progressBar;
 
     public TrackListFragment() {
     }
@@ -37,30 +33,6 @@ public class TrackListFragment extends Fragment {
         TrackListFragment trackListFragment = new TrackListFragment();
         trackListFragment.init(showControls, context, trackList);
         return trackListFragment;
-    }
-
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            updateData(context, intent);
-        }
-    };
-
-    private void updateData(final Context context, Intent intent) {
-        TrackListFragment.this.trackList = Arrays.asList(intent.getStringArrayExtra(Constants.EXTRA_TRACK_LIST));
-
-        LoadDataTask loadDataTask = new LoadDataTask();
-        loadDataTask.setContentResolver(context.getContentResolver());
-        loadDataTask.setCallback(track -> {
-            TrackListFragment.this.adapter = new TrackAdapter(track, context);
-            if (listView != null) {
-                TrackListFragment.this.listView.setAdapter(adapter);
-            }
-            progressBar.setVisibility(View.INVISIBLE);
-            listView.setVisibility(View.VISIBLE);
-            invalidate();
-        });
-        loadDataTask.execute(trackList.toArray(new String[0]));
     }
 
     void invalidate() {
@@ -72,53 +44,32 @@ public class TrackListFragment extends Fragment {
         }
     }
 
-    List<String> getTrackList() {
+    TrackList getTrackList() {
         return trackList;
     }
 
-    void init(boolean showControls, Context context, TrackList trackList) {
+    void init(boolean showControls, Context context, @NonNull TrackList trackList) {
         this.showControls = showControls;
-
-        if (trackList == null) {
-            this.trackList = new ArrayList<>();
-            this.adapter = new TrackAdapter(new ArrayList<>(), context);
-            startedWithList = false;
-        } else {
-            this.trackList = trackList.getPaths();
-            this.adapter = new TrackAdapter(trackList.getTracks(), context);
-            startedWithList = true;
-        }
-        registerUpdateReceiver(context);
+        this.trackList = trackList;
+        this.adapter = new TrackAdapter(trackList.getTracks(), context);
     }
 
-    private void registerUpdateReceiver(Context context) {
-        IntentFilter filter = new IntentFilter(Constants.ACTION_UPDATE_TRACK_LIST);
-        context.registerReceiver(receiver, filter);
-    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_tracklist, container, false);
-
-        progressBar = rootView.findViewById(R.id.track_list_progress_bar);
-        progressBar.setVisibility(View.INVISIBLE);
 
         listView = rootView.findViewById(R.id.fragment_track_list);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener((parent, view, position, id) -> {
             Intent serviceIntent = new Intent(Constants.ACTION_PLAY_FROM_LIST);
             serviceIntent.putExtra(Constants.EXTRA_PATH, ((Track) parent.getItemAtPosition(position)).getPath());
-            serviceIntent.putExtra(Constants.EXTRA_TRACK_LIST, getTrackList().toArray(new String[0]));
+            serviceIntent.putExtra(Constants.EXTRA_TRACK_LIST, trackList.getPaths().toArray(new String[0]));
             view.getContext().sendBroadcast(serviceIntent);
 
             Intent interfaceIntent = new Intent(Constants.ACTION_SHOW_PLAYER);
             view.getContext().sendBroadcast(interfaceIntent);
         });
-
-        if (!startedWithList) {
-            progressBar.setVisibility(View.VISIBLE);
-            listView.setVisibility(View.INVISIBLE);
-        }
 
         EditText searchFrame = rootView.findViewById(R.id.search_edit_text);
         ImageButton buttonShuffle = rootView.findViewById(R.id.shuffle);
@@ -133,7 +84,7 @@ public class TrackListFragment extends Fragment {
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
                     Context context = getContext();
                     if (context != null) {
-                        List<Track> trackListSearched = Utils.search(s, trackList, context.getContentResolver());
+                        List<Track> trackListSearched = Utils.search(s, trackList.getPaths(), context.getContentResolver());
                         listView.setAdapter(new TrackAdapter(trackListSearched, context));
                         if (s.length() == 0) {
                             listView.setAdapter(adapter);
@@ -146,10 +97,8 @@ public class TrackListFragment extends Fragment {
                 }
             });
             buttonShuffle.setOnClickListener(v -> {
-                Context context = getContext();
-                if (context != null) {
-                    context.sendBroadcast(new Intent(Constants.ACTION_SHUFFLE));
-                }
+//                Collections.shuffle(trackList);
+                adapter.notifyDataSetChanged();
             });
         } else {
             searchFrame.setVisibility(View.INVISIBLE);
