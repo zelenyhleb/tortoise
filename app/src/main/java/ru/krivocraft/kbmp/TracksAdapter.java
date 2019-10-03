14dc6ae025faee3be2249276a015015e72fef57c
Nikifor Fedorov <kefir.fedorov@gmail.com>
2019-10-03 10:16:33 +0300

    fixed freezes on tracks dragging

--- app/src/main/java/ru/krivocraft/kbmp/TracksAdapter.java
+++ app/src/main/java/ru/krivocraft/kbmp/TracksAdapter.java
@@ -2,9 +2,7 @@ package ru.krivocraft.kbmp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.view.DragEvent;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -12,7 +10,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.view.MotionEventCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.Collections;
